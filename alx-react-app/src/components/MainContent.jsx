daff1eb2f9e2482a5ba36b5d33fd3aeb46ec708e
amnesia2k <tilewaolatoye17@gmail.com>
2024-10-30 05:56:45 +0100

    third

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 const MainContent = () => {
   return (
     <main>
-      <p>I love to visit New York, Paris, and Tokyo</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   )
 }
