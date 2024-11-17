07fc2b5bf8a7f2a1ac644019168642064400e423
amnesia2k <tilewaolatoye17@gmail.com>
2024-11-17 10:10:09 +0100

    recipe-1

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,23 @@
+import useRecipeStore from "../stores/recipeStore";
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+  console.log(recipes);
+
+  return (
+    <div>
+      {recipes.length > 0 ? (
+        recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
+      ) : (
+        <p>No recipes available. Add one!</p>
+      )}
+    </div>
+  );
+};
+
+export default RecipeList;
