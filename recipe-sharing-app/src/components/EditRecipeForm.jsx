b4060d005b7ebb5232d1a8c9f7a1548ae703e9ed
amnesia2k <tilewaolatoye17@gmail.com>
2024-11-17 10:23:46 +0100

    recipe-1

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -10,8 +10,8 @@ const EditRecipeForm = ({ recipeId }) => {
   const [title, setTitle] = useState(recipe.title);
   const [description, setDescription] = useState(recipe.description);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
     updateRecipe(recipeId, { title, description });
   };
 
