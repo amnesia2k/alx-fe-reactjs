0732c4150fb35b9228457f16ec27972c29ccb2b7
amnesia2k <tilewaolatoye17@gmail.com>
2024-11-24 08:31:28 +0100

    advanced routing

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,6 +1,9 @@
 import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
-const ProtectedRoute = ({ isAuthenticated, children }) => {
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = useAuth(); // Use the custom hook to check authentication status
+
   return isAuthenticated ? children : <Navigate to="/" />;
 };
 
