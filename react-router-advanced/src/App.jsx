9c49965fccefac9a68898364343986bf42c9d2d3
amnesia2k <tilewaolatoye17@gmail.com>
2024-11-24 08:29:43 +0100

    advanced routing

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
             </ProtectedRoute>
           }
         />
-        <Route path="/blog/:Id" element={<BlogPost />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
   );
