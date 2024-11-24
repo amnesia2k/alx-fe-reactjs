acc801721427fffcb08e419f606babd0684a38ea
amnesia2k <tilewaolatoye17@gmail.com>
2024-11-24 08:28:10 +0100

    advanced routing

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import BlogPost from "./components/BlogPost";
@@ -7,18 +7,20 @@ import ProtectedRoute from "./components/ProtectedRoute";
 const App = () => {
   const isAuthenticated = false;
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route
-        path="/profile/*"
-        element={
-          <ProtectedRoute isAuthenticated={isAuthenticated}>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/blog/:postId" element={<BlogPost />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/blog/:postId" element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
